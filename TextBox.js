297e3d86dfdad73125b78d46262c73a41f667908
Ryan Chan <ryanchan550@gmail.com>
2019-04-02 13:56:57 -0400

    Create TextBox.js

---
+++ TextBox.js
@@ -0,0 +1,39 @@
+var pressed = true;
+function keyPressed(){
+  pressed=true;
+}
+
+function TextBox(x,y,w,h,ts){
+  this.switchedOn = false;
+  this.keyOff = false;
+  this.data = "";
+  
+  this.update = function(){
+    stroke(0);
+    if(this.switchedOn){
+      fill(150);
+    }else{
+      fill(255);
+    }
+    
+    if(this.switchedOn&&pressed){
+      if(keyCode==BACKSPACE){
+        this.data=this.data.substring(0,this.data.length-1);
+        pressed=false;
+      }else if(key.charCodeAt(0)!=16){
+        this.data+=key;
+        pressed=false;
+      }
+    }
+    
+    rect(x,y,w,h);
+    textSize(ts);
+    fill(0);
+    text(this.data,x+3,y+h-10);
+    if(mouseIsPressed&&mouseX>x&&mouseX<x+w&&mouseY>y&&mouseY<y+h){
+      this.switchedOn = true;     
+    }else if(mouseIsPressed&&(mouseX<x||mouseX>x+w||mouseY<y||mouseY>y+h)){
+      this.switchedOn = false;    
+    }
+  }
+}
