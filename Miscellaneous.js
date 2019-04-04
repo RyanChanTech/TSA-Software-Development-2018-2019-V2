59d08855c1cff46480d38da0b719ee53ac1479c2
Ryan Chan <ryanchan550@gmail.com>
2019-04-04 18:32:17 -0400

    Update Miscellaneous.js

--- Miscellaneous.js
+++ Miscellaneous.js
@@ -1,69 +1,25 @@
-/*IMPORTANT: The x and y coordinate system looks like this:
-https://openclipart.org/detail/234445/computer-math-coordinate-system
-*/
-
-//Switch parameters: (x,y,width,height,text,text size, x offset (optional))
-var mathSwitch=new CategorySwitch(10,80,280,50,"Math",40,xOffset=95);
-var physicsSwitch=new CategorySwitch(10,150,280,50,"Physics",40,xOffset=75);
-var chemistrySwitch = new CategorySwitch(10,220,280,50,"Chemistry",40,xOffset=55);
-
-var algebraSwitch = new Switch(10,380,280,50,"Simple Algebra",35);
-var derivativesSwitch = new Switch(10,450,280,50,"Derivatives",35);
-var seriesSwitch = new Switch(10,520,280,50,"Series",35);
-var matrixSwitch = new Switch(10,590,280,50,"Matrixes",35);
-
-var newtonsLawsSwitch = new Switch(10,380,280,50,"Newton's Laws",35);
-var stoichSwitch = new Switch(10,380,280,50,"Stoichiometry",35);
-
-
-var logo;
-//The setup function only runs once
-function setup() {
-  //You can ignore these. It only sets the font and canvas size
-  textFont("Trebuchet MS");
-  createCanvas(1800, 1500);
-  logo = loadImage("images/logo2.png");
-}
-
-/*The draw function loops constantly. It runs every frame, and
-since this program is running at 30 frames per second, this function
-loops 30 times a second*/
-
-function draw() {
-  background(255);
-  displayDecorations(); //This is in the Miscellaneous.js file
-
-  //update each tool based on their category
-  if(mathSwitch.switchedOn==true){
-    algebraSwitch.update();
-    derivativesSwitch.update();
-    seriesSwitch.update();
-    matrixSwitch.update();
-  }else if(physicsSwitch.switchedOn==true){
-    newtonsLawsSwitch.update();
-  }else if(chemistrySwitch.switchedOn==true){
-    stoichSwitch.update();
-  }
-
-  //If each switch is switchedOn, run the function
-  if(algebraSwitch.switchedOn==true){
-    algebra();
-  }else if(derivativesSwitch.switchedOn==true){
-    derivatives();
-  }else if(newtonsLawsSwitch.switchedOn==true){
-    newtonsLaws();
-  }else if(stoichSwitch.switchedOn==true){
-    stoich()
-  }else if(seriesSwitch.switchedOn==true){
-    series();
-  }else if(matrixSwitch.switchedOn==true){
-    matrix();
+function displayDecorations(){
+  noStroke();
+  textSize(50);
+  fill(0);
+  text("TSA Question Generator",320,70);
+  fill(30,30,30,100);
+  rect(0,0,300,300);
+  fill(100,100,100,100);
+  rect(0,300,300,1000);
+  fill(255,255,255);
+  textSize(40);
+  text("Categories",10,50);
+  text("Generators",10,350);
+  if(mathSwitch.switchedOn){
+    fill(100,150,200,100);
+  }else if(physicsSwitch.switchedOn){
+    fill(100,200,100,100);
+  }else if(chemistrySwitch.switchedOn){
+    fill(200,100,200,100);
   }else{
-    image(logo,300,80);
+    fill(100,100,100,100);
   }
-
-  //update the category switches so they show up and functions.
-  mathSwitch.update();
-  physicsSwitch.update();
-  chemistrySwitch.update();
+  rect(1350,0,200,1000);
+  stroke(0);
 }
