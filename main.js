54eb8f720cc70e4e524fb1b120696e36a5784273
Ryan Chan <ryanchan550@gmail.com>
2019-04-04 18:33:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,16 +8,6 @@ var chemistrySwitch = new CategorySwitch(10,220,280,50,"Chemistry",40,xOffset=55
 
 var algebraSwitch = new Switch(10,380,280,50,"Simple Algebra",35);
 var derivativesSwitch = new Switch(10,450,280,50,"Derivatives",35);
-var seriesSwitch = new Switch(10,520/*IMPORTANT: The x and y coordinate system looks like this:
-https://openclipart.org/detail/234445/computer-math-coordinate-system*/
-
-//Switch parameters: (x,y,width,height,text,text size, x offset (optional))
-var mathSwitch=new CategorySwitch(10,80,280,50,"Math",40,xOffset=95);
-var physicsSwitch=new CategorySwitch(10,150,280,50,"Physics",40,xOffset=75);
-var chemistrySwitch = new CategorySwitch(10,220,280,50,"Chemistry",40,xOffset=55);
-
-var algebraSwitch = new Switch(10,380,280,50,"Simple Algebra",35);
-var derivativesSwitch = new Switch(10,450,280,50,"Derivatives",35);
 var seriesSwitch = new Switch(10,520,280,50,"Series",35);
 var matrixSwitch = new Switch(10,590,280,50,"Matrixes",35);
 
