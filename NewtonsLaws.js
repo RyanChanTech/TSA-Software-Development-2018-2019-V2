fc61e134bf39630e935440d2d52cd888ceb8ea61
hopper19 <37946259+hopper19@users.noreply.github.com>
2019-04-05 20:42:44 -0400

    Small fixes
    Sigma, Increase Newtons' explanation textsize

--- NewtonsLaws.js
+++ NewtonsLaws.js
@@ -55,7 +55,7 @@ function newtonsLaws(){
   }
 
   text("Work/Explanation: ",750,200);
-  textSize(20)
+  textSize(25)
   text(nLExplanation,750,200)
   textSize(30);
   text("Feedback:" + nLFeedback,320,610);
@@ -69,8 +69,10 @@ function nLGeneratePulleyAcceleration(){
   nLMass2 = Math.round(((Math.random() * 25)+1)*100)/100;
   var acceleration = (nLMass2*9.8)/(nLMass1+nLMass2);
   acceleration=Math.round(acceleration*100)/100;
-  nLQuestion="Mass 1 is " + nLMass1 + "kg and Mass 2 is " + nLMass2 + "kg. They are connected by a string and pulley. \nWhat is the acceleration of the two masses? Ignore friction.";
-  nLTempExplanation="\n\nStep 1- Use F=ma=sum of all forces\n\nStep 2- Forces on m2 are gravity and tension,so m2a=m2g-T\n\nStep 3- force on m1 is only tension, so m1a=T\n\nStep 4- Use substitution to get m2a=m2g-m1a\n\nStep 5-Solve for a to get: " + acceleration;
+  nLQuestion="Mass 1 is " + nLMass1 + "kg and Mass 2 is " + nLMass2 + "kg. They are connected by a string and pulley. "+
+  "\nWhat is the acceleration of the two masses? Ignore friction.";
+  nLTempExplanation="\n\nStep 1- Use F = ma = sum of all forces\n\nStep 2- Forces on m2 are gravity and tension,\n           so m2a = m2g - T"+
+  "\n\nStep 3- Force on m1 is only tension, so m1a = T\n\nStep 4- Use substitution to get m2a = m2g - m1a\n\nStep 5- Solve for a to get: " + acceleration;
 
   nLAnswerButtonIndex = Math.floor(Math.random() * 3);
   for(var i=0;i<3;i++){
@@ -85,11 +87,14 @@ function nLGeneratePulleyAcceleration(){
 function nLGeneratePulleyTension(){
   nLMass1 = Math.floor(Math.random() * 10)+1;
   nLMass2 = Math.floor(Math.random() * 10)+1;
-  nLQuestion="Mass 1 is " + nLMass1 + "kg and Mass 2 is " + nLMass2 + "kg. They are connected by a string and pulley. \nWhat is the tension of the string? Use 9.8m/s^2 for gravity and ignore friction."
+  nLQuestion="Mass 1 is " + nLMass1 + "kg and Mass 2 is " + nLMass2 + "kg. They are connected by a string and pulley. "+
+  "\nWhat is the tension of the string? Use 9.8m/s^2 for gravity and ignore friction."
   var tension = 9.8*(nLMass1+nLMass2);
   tension=Math.round(tension*100)/100;
 
-  nLTempExplanation="\n\nStep 1- Use F=ma=sum of all forces\n\nStep 2- Forces on m2 are gravity and tension,so m2a=m2g-T\n\nStep 3- force on m1 is only tension, so m1a=T\n\nStep 4- Solve for a from Step 3 to get a=T/m1 \n\nStep 5- Use substitution to get m2(T/m1)=m2g-T\n\nStep 6- Solve for T to get: " + tension;
+  nLTempExplanation="\n\nStep 1- Use F = ma = sum of all forces\n\nStep 2- Forces on m2 are gravity and tension,\n           so m2a = m2g - T"+
+  "\n\nStep 3- Force on m1 is only tension, so m1a = T\n\nStep 4- Solve for a from Step 3 to get a = T / m1 "+
+  "\n\nStep 5- Use substitution to get\n           m2(T  / m1) = m2g - T\n\nStep 6- Solve for T to get: " + tension;
   nLAnswerButton = Math.floor(Math.random() * 3);
   for(var i=0;i<3;i++){
     if(i==nLAnswerButton){
@@ -126,7 +131,7 @@ function nLGenerateSlideAcceleration(){
   var acceleration=(9.8*Math.sin(radians(theta))-friction*9.8*Math.cos(radians(theta))).toFixed(2);
   nLQuestion="The mass of the box is "+ mass+ "kg, the angle is " + theta +" degrees, and the friction coefficient is "+ friction+
   ".\nWhat is the acceleration of the box sliding down the incline?";
-  nLTempExplanation="\n\nForces acting on the box in the direction of the slide:\n   F = gravity - friction = Fg - Fμ\n     = mgsinθ - μmgcosθ = ma"+
+  nLTempExplanation="\n\nForces acting on the box in the direction\nof the slide:\n   F = gravity - friction = Fg - Fμ\n     = mgsinθ - μmgcosθ = ma"+
   "\n\nCancel m on both sides of the equation:\n   a = gsinθ - μgcosθ\n\nSubtitute: g =9.8m/s ; θ = " + theta + " ; μ = " + friction +
   "\n   to get: Acceleration = a = " + acceleration;
 
