7598cd2c1aa88247ec425cace8c00d8cb7893294
hopper19 <37946259+hopper19@users.noreply.github.com>
2019-05-30 13:21:46 -0400

    Update Integrals.js

--- Integrals.js
+++ Integrals.js
@@ -4,9 +4,12 @@ var integralsTextBox = new TextBox(320,150,350,40,30);
 var integralsCheckButton = new Button(380,200,180,40,"Check",30,xOffset=50);
 var integralsNewQuestionButton = new Button(370,250,220,40,"New Question",30,xOffset=15);
 var integralsGenerateQuestion = true;
-var integralsAnswer = 0;
+var integralsTopic = "";
+var integralsQuestion = "";
+var integralsAnswer = "";
 var integralsFeedback="";
 var integralsExplanation = "";
+var integralsShowExplanation = false;
 
 function integrals(){
   fill(255,238,153,100);
@@ -17,59 +20,81 @@ function integrals(){
   integralsCheckButton.update();
   integralsNewQuestionButton.update();
 
-  /*If a question has not been generated yet,make 2 numbers
-  between 0 and 0 and between 0make the answer. Then set generatedQuestion
-  to true at the end so it does not generate a new one
-  before the current one is answered*/
+  /* Type =
+
+    SubSubType = coefficient or not*/
   if(integralsGenerateQuestion == true){
-    var randomIntegralType = Math.floor(random(0,1));
-    var randomIntegralSubType = Math.floor(random(0,3));
+    //application or not
+    var randomQuestionType = Math.floor(random(0,1));
+    //single or double or triple
+    var randomIntegralsType = Math.floor(random(0,1));
+    //polynomial or e or sin or cos or const
+    var randomIntegralsSubType = Math.floor(random(0,5));
+
+    var randomIntegralsCoefficient = Math.floor(random(2,9));
+    //definite or indef
+    var randomIntegralsNumeric = Math.floor(random(0,1));
+    var randomLowerBound = Math.floor(random(0,5));
+    var randomUpperBound = Math.floor(random(5,10));
 
     // it's a single integral
-    if (randomIntegralType == 0){
-      if (randomIntegralSubType == 0 or 1){
-        var randomFunctionType = Math.floor(random(0,))
+    if (randomQuestionType == 0){
+      if (randomIntegralsType == 0){
+        if (randomIntegralsSubType == 0){
+          integralsQuestion = randomIntegralsCoefficient + "x^" + (randomIntegralsCoefficient-1);
+          integralsAnswer = "x^" + randomIntegralsCoefficient;
+        } else if (randomIntegralsSubType == 1){
+          integralsQuestion = randomIntegralsCoefficient + "e^x";
+          integralsAnswer = randomIntegralsCoefficient + "e^x";
+        } else if (randomIntegralsSubType == 2){
+          integralsQuestion = randomIntegralsCoefficient + "sin(x)";
+          integralsAnswer = "-" + randomIntegralsCoefficient + "cos(x)";
+        } else if (randomIntegralsSubType == 3){
+          integralsQuestion = randomIntegralsCoefficient + "cos(x)";
+          integralsAnswer = randomIntegralsCoefficient + "sin(x)";
+        } else if (randomIntegralsSubType == 4){
+          integralsQuestion = randomIntegralsCoefficient;
+          integralsAnswer = randomIntegralsCoefficient + "x";
+        }
       }
-
-      var integralsRandomNumberOne = Math.floor(Math.random() * 50);
-      var integralsRandomNumberTwo = Math.floor(Math.random() * 50);
-      integralsAnswer=integralsRandomNumberTwo-integralsRandomNumberOne;
-    }
-
+      integralsTopic = "Calculate the following integral:";
     integralsGenerateQuestion = false;
+    }
+    console.log(integralsQuestion);
+    console.log(integralsAnswer);
   }
 
   /*If the integralsNewQuestionButon is clicked, set integralsGenerateQuestion
   to true, so it generates a new question in the next frame*/
   if(integralsNewQuestionButton.clicked==true){
     integralsTextBox.data="";
-    integralsExplanation = "";
-    integralsFeedback = "";
     integralsGenerateQuestion = true;
+    integralsShowExplanation = false;
   }
 
   /*check if the user's answer is right or not when the
   integralsCheckButton is clicked*/
   if(integralsCheckButton.clicked==true){
-    /*The parseInt() function turns integralsTextBox.data, which is a string (or text)
-    data type into an int datatype so they can be compared*/
-    if(parseInt(integralsTextBox.data)==integralsAnswer){
+    integralsShowExplanation = true;
+    /*The parseFloat() function turns integralsTextBox.data, which is a string (or text)
+    data type into a float datatype so they can be compared*/
+    if((integralsTextBox.data)==integralsAnswer){
       integralsFeedback="Correct!";
     }else{
       integralsFeedback="Try again"
     }
-
-    integralsExplanation="Subtract: " + integralsRandomNumberTwo+" - "+integralsRandomNumberOne+" = "+integralsAnswer;
   }
 
   //Display the output, feedback, and work
-  text(integralsRandomNumberOne + " + x = " + integralsRandomNumberTwo,320,120);
-  text("Feedback: " + integralsFeedback,320,330);
-
+  text("Feedback: ",320,330);
   text("Work/Explanation:",800,200);
-  text(integralsExplanation,800,250);
+  if (integralsShowExplanation){
+      text(integralsExplanation,800,250);
+      text(integralsFeedback,470,330);
+  }
 }
 
 
 
 //∫
+//]
