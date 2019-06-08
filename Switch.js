4afb883018225e46865c132f9e8723da637a447b
ChanR19 <ryanchan550@gmail.com>
2019-06-08 12:47:57 -0400

    Update Switch.js

--- Switch.js
+++ Switch.js
@@ -1,4 +1,6 @@
-function Switch(x,y,w,h,t,ts,xOffset=5){
+//Switch(x Position, y Position, width, height, [x boundary point 1, y boundary point 1,x Boundary point 2, y boundary point 2] (OPTIONAL to change. set to [0,0,300,1080 by default]), text, text size, x Offset)
+//Regarding categoryBoundary: All switches within this boundary will be treated as a category, i.e. when one switch in the category is selected, the others within the same category will be deselected, but all switches outside of the categoryBoundary will be unaffected
+function Switch(x,y,w,h,categoryBoundary=[0,0,300,1080],t,ts,xOffset=5){
   this.switchedOn = false;
 
   this.update = function(){
